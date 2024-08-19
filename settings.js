ff95b312596c85656dfd5f4ea2e1b6a167eee57c
Druskiimask45 <slycosilver@gmail.com>
2024-08-19 23:02:51 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -11,7 +11,7 @@ const FOOTERNAME = '> ＶＡＪＩＲＡ -  ＭＤ - Ｖ4';
 global.owner = process.env.OWNER_NUMBER  || '254110421003' ;  // ADD YOUR NUMBER WITHOUT +
     
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=PIsBDQza#WXvyVAuwUZo0gc6od9V2X7SL_8w_HujkdFpfhkrDNPY ' ,
+SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=uVUAzJjS#JeMuInfRIq-DEpgt72rjGQFWpGkZci3Ahzu2kgcZpAg ' ,
 travaSend: process.env.TRAVA_SEND || '25' ,   
 MODERATORS : process.env.MODERATORS === undefined ? "254110421003" : process.env.MODERATORS ,    
 MAX_SIZE: process.env.MAX_SIZE === undefined ? '1536': process.env.MAX_SIZE,/*add this in megabytes*/    
